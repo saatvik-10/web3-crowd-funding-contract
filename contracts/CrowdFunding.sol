e8eebaeeaf7816ce41312fbcfda46c9c992e3e57
saatvik-10 <saatvikmadan529@gmail.com>
2025-05-19 23:52:49 +0530

    added onlyOwner, addTier and removeTier function

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -8,6 +8,19 @@ contract CrowdFunding {
     uint256 public deadline;
     address public owner;
 
+    struct Tier {
+        string name;
+        uint256 amount;
+        uint256 backers;
+    }
+
+    Tier[] public tiers;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
     constructor(
         string memory _name,
         string memory _description,
@@ -21,13 +34,26 @@ contract CrowdFunding {
         owner = msg.sender;
     }
 
-    function fund() public payable {
-        require(msg.value > 0, "Fund amount must be greater than 0!");
+    function fund(uint256 _tierIndex) public payable {
         require(block.timestamp < deadline, "Campaign has ended!");
+        require(_tierIndex < tiers.length, "Tier does not exist!");
+        require(msg.value == tiers[_tierIndex].amount, "Incorrect Amount!");
+
+        tiers[_tierIndex].backers++;
     }
 
-    function withdraw() public {
-        require(msg.sender == owner, "Only owner is allowed to withdraw!");
+    function addTier(string memory _name, uint256 _amount) public onlyOwner {
+        require(_amount > 0, "Amount must be greater than 0!");
+        tiers.push(Tier(_name, _amount, 0));
+    }
+
+    function removeTier(uint256 _index) public onlyOwner {
+        require(_index < tiers.length, "Tier does not exist!");
+        tiers[_index] = tiers[tiers.length - 1];
+        tiers.pop();
+    }
+
+    function withdraw() public onlyOwner {
         require(
             address(this).balance >= goal,
             "Goal has not been reached yet!"
